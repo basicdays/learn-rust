5e0d3ba33a5de22a338968ab393eab6e8a07445b
basicdays <info@basicdays.com>
2020-05-09 23:26:10 -0500

    feat: convert to vscode; add some projects

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,24 @@
+mod back_of_house;
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+fn serve_order() {}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
